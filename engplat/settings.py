0da0614f44f05590f240005e6602de2dc083e140
Fabio Zanetti <fczanetti@hotmail.com>
2023-12-29 11:23:11 -0300

    primeiras configurações para deploy

--- engplat/settings.py
+++ engplat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +26,7 @@ SECRET_KEY = 'django-insecure-mnj9%gjau8lq3v!8^nq=*42c)_hnx)g-k%!3s0_$9!ys63b(g=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
