870291242b94ba2e8642027a92cbe80080e13cf0
Fabio Zanetti <fczanetti@hotmail.com>
2024-01-04 11:44:54 -0300

    django-debug-toolbar instalado

--- engplat/urls.py
+++ engplat/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('engplat.base.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(
+        path('__debug__/', include('debug_toolbar.urls')),
+    )
