9cd0ad1d738c93cbaacc66f2c1a4537dbacb9327
Fabio Zanetti <fczanetti@hotmail.com>
2024-01-08 16:45:11 -0300

    videos inseridos na pagina de podcasts

--- engplat/podcasts/admin.py
+++ engplat/podcasts/admin.py
@@ -1,3 +1,9 @@
-# from django.contrib import admin
+from django.contrib import admin
+from engplat.podcasts.models import Podcast
 
-# Register your models here.
+
+@admin.register(Podcast)
+class PodcastAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'slug', 'data_gravacao', 'plat_id']
+    prepopulated_fields = {'slug': ['titulo']}
+    ordering = ['-data_gravacao']
--- engplat/podcasts/models.py
+++ engplat/podcasts/models.py
@@ -1,3 +1,16 @@
-# from django.db import models
+from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Podcast(models.Model):
+    titulo = models.CharField(max_length=64)
+    slug = models.SlugField(unique=True)
+    data_gravacao = models.DateField()
+    plat_id = models.CharField(max_length=32)
+    descricao = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('podcasts:podcast', kwargs={'slug': self.slug})
