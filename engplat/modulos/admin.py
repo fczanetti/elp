11d147cb7a7720444c0bf333f77362ffdf79be37
Fabio Zanetti <fczanetti@hotmail.com>
2024-01-14 16:56:15 -0300

    modelo criado, migrado e adicionado em admin

--- engplat/modulos/admin.py
+++ engplat/modulos/admin.py
@@ -1,3 +1,9 @@
-# from django.contrib import admin
+from django.contrib import admin
+from engplat.modulos.models import Modulo
+from ordered_model.admin import OrderedModelAdmin
 
-# Register your models here.
+
+@admin.register(Modulo)
+class ModuloAdmin(OrderedModelAdmin):
+    list_display = ['titulo', 'slug', 'publico', 'order']
+    prepopulated_fields = {'slug': ['titulo']}
--- engplat/modulos/tests/test_indice_modulos.py
+++ engplat/modulos/tests/test_indice_modulos.py
@@ -1,11 +1,17 @@
 import pytest
 from django.urls import reverse
-
 from engplat.django_assertions import assert_contains
+from model_bakery import baker
 
 
 @pytest.fixture
-def resp(client):
+def modulos(db):
+    modulos = baker.make('Modulo', _quantity=3)
+    return modulos
+
+
+@pytest.fixture
+def resp(client, modulos):
     return client.get(reverse('modulos:indice_modulos'))
 
 
@@ -15,3 +21,23 @@ def test_status_code_indice_modulo(resp):
 
 def test_titulo_indice_modulos(resp):
     assert_contains(resp, '<title>Módulos - Índice</title>')
+
+
+def test_titulos_modulos(resp, modulos):
+    for modulo in modulos:
+        assert_contains(resp, modulo.titulo)
+
+
+def test_publico_modulos(resp, modulos):
+    for modulo in modulos:
+        assert_contains(resp, modulo.publico)
+
+
+def test_descricao_modulos(resp, modulos):
+    for modulo in modulos:
+        assert_contains(resp, modulo.descricao)
+
+
+def test_link_modulos(resp, modulos):
+    for modulo in modulos:
+        assert_contains(resp, reverse('modulos:detalhe', args=(modulo.slug,)))
