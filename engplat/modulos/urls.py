8dc2123c9579a2c4b6c9128874dafda8d39cefbc
Fabio Zanetti <fczanetti@hotmail.com>
2024-01-15 11:32:23 -0300

    modelo aulas criado

--- engplat/modulos/urls.py
+++ engplat/modulos/urls.py
@@ -4,5 +4,5 @@ from engplat.modulos import views
 app_name = 'modulos'
 urlpatterns = [
     path('', views.indice_modulos, name='indice_modulos'),
-    path('<slug:slug>', views.detalhe_modulos, name='detalhe'),
+    path('<slug:slug>', views.detalhe_modulo, name='detalhe_modulo'),
 ]
--- engplat/modulos/views.py
+++ engplat/modulos/views.py
@@ -7,5 +7,7 @@ def indice_modulos(request):
     return render(request, 'modulos/indice_modulos.html', {'modulos': modulos})
 
 
-def detalhe_modulos(request):
-    return render(request, 'modulos/detalhe.html')
+def detalhe_modulo(request, slug):
+    modulo = facade.buscar_modulo(slug)
+    aulas = facade.listar_aulas_de_modulo_ordenadas(modulo)
+    return render(request, 'modulos/detalhe_modulo.html', {'modulo': modulo, 'aulas': aulas})
