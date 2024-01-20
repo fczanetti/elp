5a7c19990c8cf39cfd4d2fabf886208ef2582c52
Fabio Zanetti <fczanetti@hotmail.com>
2024-01-20 11:01:12 -0300

    formularios esqueceu senha e troca de senha criados

--- engplat/base/urls.py
+++ engplat/base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from engplat.base import views
+from engplat.base.views import CustomPasswordResetView, CustomPasswordChangeView
 
 app_name = 'base'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.UserLoginView.as_view(), name='user_login'),
+    path('accounts/login/', views.UserLoginView.as_view(), name='login'),
+    path('accounts/password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_change/', CustomPasswordChangeView.as_view(), name='password_change'),
 ]
--- engplat/base/views.py
+++ engplat/base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordChangeView
 
 
 def home(request):
@@ -9,3 +9,11 @@ def home(request):
 class UserLoginView(LoginView):
     template_name = 'registration/user_login.html'
     next_page = '/'
+
+
+class CustomPasswordResetView(PasswordResetView):
+    template_name = 'registration/password_reset.html'
+
+
+class CustomPasswordChangeView(PasswordChangeView):
+    template_name = 'registration/password_change.html'
