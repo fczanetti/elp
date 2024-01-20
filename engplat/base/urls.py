51a3b7a1460078bdd2b9fde7a83f4c360ac1cfa6
Fabio Zanetti <fczanetti@hotmail.com>
2024-01-19 21:07:21 -0300

    formulario de login criado

--- engplat/base/urls.py
+++ engplat/base/urls.py
@@ -3,5 +3,6 @@ from engplat.base import views
 
 app_name = 'base'
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('login/', views.UserLoginView.as_view(), name='user_login'),
 ]
--- engplat/base/views.py
+++ engplat/base/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
 
 def home(request):
     return render(request, 'base/home.html')
+
+
+class UserLoginView(LoginView):
+    template_name = 'registration/user_login.html'
+    next_page = '/'
