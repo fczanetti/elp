9d943d39b9f4ffcfd5768091020fb088c11dc0ba
Fabio Zanetti <fczanetti@hotmail.com>
2024-01-21 21:23:34 -0300

    formularios email enviado e nova definicao de senha ajustados

--- engplat/base/urls.py
+++ engplat/base/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from engplat.base import views
-from engplat.base.views import CustomPasswordResetView, CustomPasswordChangeView
+
 
 app_name = 'base'
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/login/', views.UserLoginView.as_view(), name='login'),
     path('accounts/logout/', views.UserLogoutView.as_view(), name='logout'),
-    path('accounts/password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
-    path('accounts/password_change/', CustomPasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_change/', views.CustomPasswordChangeView.as_view(), name='password_change'),
     path('accounts/password_alterado', views.password_alterado, name='password_alterado'),
+    path('accounts/password_reset_confirmed', views.CustomPasswordResetDoneView.as_view(),
+         name='password_reset_confirmed'),
+    path('accounts/reset/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
 ]
